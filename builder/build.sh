aaa5c368f7c2a469fdabb4ceb09a4d3bee599781
sheng2216 <sheng.lyu@rakwireless.com>
2023-03-09 10:25:10 +0000

    modify as923 channel plan

--- builder/build.sh
+++ builder/build.sh
@@ -8,6 +8,7 @@ REMOTE_TAG=${REMOTE_TAG:-f8c9335} # This is latest commit on master branch at 20
 # Clone 
 if [[ ! -d repo ]]; then
     git clone https://github.com/RAKWireless/rak_common_for_gateway repo
+    #git clone https://github.com/Sheng2216/rak_common_for_gateway_fork.git repo
 fi
 
 # Check out tag
@@ -30,5 +31,21 @@ for FOLDER in ${FOLDERS[@]}; do
 done
 popd
 
+
 # Get out of repo folder
 popd
+
+git clone https://github.com/Sheng2216/rak_common_for_gateway_new_fork.git new_repo
+
+cp -r new_repo/lora/rak2246/global_conf repo/lora/rak2246/global_conf
+cp -r new_repo/lora/rak2247_spi/global_conf repo/lora/rak2247_spi
+cp -r new_repo/lora/rak2247_usb/global_conf repo/lora/rak2247_usb
+cp -r new_repo/lora/rak2287/global_conf_i2c repo/lora/rak2287
+cp -r new_repo/lora/rak2287/global_conf_uart repo/lora/rak2287
+cp -r new_repo/lora/rak2287/global_conf_usb repo/lora/rak2287
+cp -r new_repo/lora/rak5146/global_conf_i2c repo/lora/rak5146
+cp -r new_repo/lora/rak5146/global_conf_uart repo/lora/rak5146
+cp -r new_repo/lora/rak5146/global_conf_usb repo/lora/rak5146
+cp -r new_repo/lora/rak7243/global_conf_i2c repo/lora/rak7243
+cp -r new_repo/lora/rak7243/global_conf_uart repo/lora/rak7243
+rm -rf new_repo
